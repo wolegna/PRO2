2582659133e6f3b2bc1ce628e04fd9941ced755b
wolegna <58250102+wolegna@users.noreply.github.com>
2021-05-19 00:36:55 +0300

    js works now
    found out why js was not working, renamed js file in <link>. added functionalities

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-var input = document.getElementsById('inputEM');
+var input = document.getElementById("inputEM");
 
 input.oninput = function () {
-	var dist = document.getElementById('inputEM').value;
-	document.getElementById('outputTime').innerHTML = dist * 1.045;
+	var dist = document.getElementById("inputEM").value;
+	document.getElementById("outputTime").innerHTML = dist * 25;
 };
 
 var rocket = document.getElementById("rocket");
 
-rocket.onclick = function() {
-	
-	rocket.style.left = "250"
+var miles = document.getElementById("miles")
+
+var pos = 10;
+
+var dist = 225000000;
+
+var trip = 0;
+
+
+window.onkeypress = function () {
+
+	var spbar = event.keyCode;
+
+	if (pos == 65) {
+		rocket.style.transform = "rotate(270deg)";
+	} else {
+		if (spbar == 32) {
+			pos += 11;
+			rocket.style.left = (pos + "vw");
+			trip += dist / 5;
+			miles.innerHTML = trip + "/225000000 KM";
+			console.log(trip);
+		}
+	}
+
 }
