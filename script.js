c72c47736bf04a7dfb14e98d98be631177f01568
wolegna <58250102+wolegna@users.noreply.github.com>
2021-05-19 11:28:09 +0300

    Final Commit
    Site is working, javascript acts as expected, all gucci now.
    Added food stock calculator as well

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var input = document.getElementById("inputEM");
-
-input.oninput = function () {
-	var dist = document.getElementById("inputEM").value;
-	document.getElementById("outputTime").innerHTML = dist * 25;
-};
+var inputTime = document.getElementById("inputED");
 
 var rocket = document.getElementById("rocket");
 
-var miles = document.getElementById("miles")
+var miles = document.getElementById("miles");
+
+var inputFood = document.getElementById("inputFD");
+
+var button = document.getElementById("calculate");
+
+var outputFood = document.getElementById("outputFD");
 
 var pos = 10;
 
@@ -15,16 +16,27 @@ var dist = 225000000;
 
 var trip = 0;
 
+button.onclick = function () {
+	var z = 100 - inputFood.value;
+	outputFood.innerHTML = z * 36;
+};
+
+
+inputTime.oninput = function () {
+	var dist = document.getElementById("inputED").value;
+	document.getElementById("outputTime").innerHTML = dist * 25;
+};
 
 window.onkeypress = function () {
-
 	var spbar = event.keyCode;
-
-	if (pos == 65) {
+	if (pos == 75) {
+		document.getElementById("end").style.visibility = "visible";
+		document.getElementById("flight").style.visibility = "hidden";
 		rocket.style.transform = "rotate(270deg)";
+
 	} else {
 		if (spbar == 32) {
-			pos += 11;
+			pos += 13;
 			rocket.style.left = (pos + "vw");
 			trip += dist / 5;
 			miles.innerHTML = trip + "/225000000 KM";
